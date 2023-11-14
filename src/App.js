eed27cc7cd5c737d60a7c2040c0391f385f0bba9
Olga Kalacheva <kalach2256954@gmail.com>
2023-11-14 18:08:21 +0400

    add function get-fetch

--- src/App.js
+++ src/App.js
@@ -1,86 +1,103 @@
 import { Header } from "./components/Header";
 import { Form } from "./components/Form";
 import style from "./App.module.css";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { DataList } from "./components/DataList";
 import { Filter } from "./components/Filter";
 
-const data = [
-  {
-    taskTitle: "Drink coffee",
-    id: "1",
-    key: "1",
-    isImportant: false,
-    isDone: false,
-  },
-  {
-    taskTitle: "Go to the gym",
-    id: "2",
-    key: "2",
-    isImportant: false,
-    isDone: false,
-  },
-];
+const modifyData = (obj) => {
+  console.log("modifyData  obj:", obj);
+  const arrOfData = [];
+
+  for (const objKey in obj) {
+    arrOfData.push({
+      id: objKey,
+      isDone: obj[objKey].isDone,
+      isImportant: obj[objKey].isImportant,
+      taskTitle: obj[objKey].taskTitle,
+    });
+  }
+  console.log("modifyData  arrOfData:", arrOfData);
+
+  return arrOfData;
+};
 
 function App() {
-  const [dataState, setData] = useState(data);
+  const [dataState, setData] = useState([]);
   const [activeFilter, setActiveFilter] = useState("all");
   const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState(null);
 
-  const changeDataHandler = useCallback(
-    (newTask) => {
-      setData((prevDataState) => {
-        return [...prevDataState, newTask];
-      });
-    },
-    [setData]
-  );
+  const changeDataHandler = useCallback(async () => {
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://http-todo-default-rtdb.europe-west1.firebasedatabase.app/todos.json"
+      );
+      if (!response.ok) {
+        throw new Error("Ошибка запроса.");
+      }
+      const data = await response.json();
+      console.log("changeDataHandler  data:", data);
 
-  const deleteItemHandler = (delitedItemId) => {
-    const indexDelitItem = dataState.findIndex(
-      (task) => task.id === delitedItemId
-    );
+      const newData = modifyData(data);
+      console.log("changeDataHandler  newData:", newData);
 
-    const changedData = [...dataState];
-    changedData.splice(indexDelitItem, 1);
-    setData(changedData);
-  };
+      setData(newData);
+    } catch (err) {
+      setError(err.message || "Что-то пошло не так...");
+    }
+  }, []);
 
-  const changeImportantTaskHandler = (importantID) => {
-    const indexImportantItem = dataState.findIndex(
-      (task) => task.id === importantID
-    );
+  useEffect(() => {
+    changeDataHandler();
+  }, [changeDataHandler]);
 
-    const targetTAsk = dataState[indexImportantItem];
-    const updatedImportantTask = {
-      ...targetTAsk,
-      isImportant: !targetTAsk.isImportant,
-    };
+  // const deleteItemHandler = (delitedItemId) => {
+  //   const indexDelitItem = dataState.findIndex(
+  //     (task) => task.id === delitedItemId
+  //   );
 
-    const changedData = [...dataState];
-    changedData.splice(indexImportantItem, 1, updatedImportantTask);
-    setData(changedData);
-  };
+  //   const changedData = [...dataState];
+  //   changedData.splice(indexDelitItem, 1);
+  //   setData(changedData);
+  // };
 
-  const changeDoneTaskHandler = (doneId) => {
-    const indexDoneItem = dataState.findIndex((task) => task.id === doneId);
+  // const changeImportantTaskHandler = (importantID) => {
+  //   const indexImportantItem = dataState.findIndex(
+  //     (task) => task.id === importantID
+  //   );
 
-    const targetTAsk = dataState[indexDoneItem];
-    const updatedDoneTask = {
-      ...targetTAsk,
-      isDone: !targetTAsk.isDone,
-    };
+  //   const targetTAsk = dataState[indexImportantItem];
+  //   const updatedImportantTask = {
+  //     ...targetTAsk,
+  //     isImportant: !targetTAsk.isImportant,
+  //   };
 
-    const changedData = [...dataState];
-    changedData.splice(indexDoneItem, 1, updatedDoneTask);
-    setData(changedData);
-  };
+  //   const changedData = [...dataState];
+  //   changedData.splice(indexImportantItem, 1, updatedImportantTask);
+  //   setData(changedData);
+  // };
 
-  const doneTaskCount = dataState.reduce((sum, current) => {
-    return current.isDone ? sum + 1 : sum + 0;
-  }, 0);
+  // const changeDoneTaskHandler = (doneId) => {
+  //   const indexDoneItem = dataState.findIndex((task) => task.id === doneId);
 
-  const totalToDoTask = dataState.length - doneTaskCount;
+  //   const targetTAsk = dataState[indexDoneItem];
+  //   const updatedDoneTask = {
+  //     ...targetTAsk,
+  //     isDone: !targetTAsk.isDone,
+  //   };
+
+  //   const changedData = [...dataState];
+  //   changedData.splice(indexDoneItem, 1, updatedDoneTask);
+  //   setData(changedData);
+  // };
+
+  // const doneTaskCount = dataState.reduce((sum, current) => {
+  //   return current.isDone ? sum + 1 : sum + 0;
+  // }, 0);
+
+  // const totalToDoTask = dataState.length - doneTaskCount;
 
   const filteredData = (tagFilter) => {
     if (tagFilter === "all") {
@@ -104,18 +121,21 @@ function App() {
 
   return (
     <div className={style.wrap}>
-      <Header doneTaskCount={doneTaskCount} totalToDoTask={totalToDoTask} />
-      <Filter
+      {/* <Header doneTaskCount={doneTaskCount} totalToDoTask={totalToDoTask} /> */}
+      {/* <Filter
         inputValue={inputValue}
         setInputValue={setInputValue}
         changeState={setActiveFilter}
         activeFilter={activeFilter}
-      />
+      /> */}
       <DataList
-        data={filteredDataStateByInputValue}
-        onDeleteItem={deleteItemHandler}
-        onChangeImportantTask={changeImportantTaskHandler}
-        onChangeDoneTask={changeDoneTaskHandler}
+        data={dataState}
+        // onDeleteItem={deleteItemHandler}
+        onDeleteItem={() => console.log("onDeleteItem")}
+        // onChangeImportantTask={changeImportantTaskHandler}
+        onChangeImportantTask={() => console.log("onChangeImportantTask")}
+        // onChangeDoneTask={changeDoneTaskHandler}
+        onChangeDoneTask={() => console.log("onChangeDoneTask")}
       />
       <Form onAddNewData={changeDataHandler} />
     </div>
